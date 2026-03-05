a6eda0b2146ace0cd25a21ec7b8d00024f6b1d7e
GabrielRiffoJ <gabrielriffo22@gmail.com>
2026-03-05 15:32:57 +0100

    Fixed function benchmark

--- examples/Benchmark_sig_constructor.jl
+++ examples/Benchmark_sig_constructor.jl
@@ -1,52 +1,64 @@
 using BenchmarkTools
 using Statistics 
-
-
-# change default for `seconds` to 2.5
-BenchmarkTools.DEFAULT_PARAMETERS.seconds = 400
-
-BenchmarkTools.DEFAULT_PARAMETERS.samples = 100
+using Random
+using Oscar
+using SignatureTensors
 
 
 function benchmark_signature(ds::Vector{Int}, ms::Vector{Int}, k::Int; 
-                             signature_path::Symbol=:pwln,seq_type::Symbol=:iis, num_samples::Int=100)
+                             signature_path::Symbol=:pwln,
+                             seq_type::Symbol=:iis,
+                             num_samples::Int=100,algorithm::Symbol = :default)
 
-
-    results = Dict{Tuple{Int,Int}, Float64}()
+    # Diccionarios para guardar resultados
+    wall_time = Dict{Tuple{Int,Int}, Float64}()
+    gc_time   = Dict{Tuple{Int,Int}, Float64}()
+    memory    = Dict{Tuple{Int,Int}, Float64}()
 
     for d in ds
         for m in ms
-            T = TruncatedTensorAlgebra(QQ, d, k,seq_type)  
+            T = TruncatedTensorAlgebra(QQ, d, k, seq_type)
+            A = QQ.(rand(-20:20, d, m))
 
-            benchset = @benchmarkable sig(T, signature_path, coef=QQ.(rand(-20:20, d, m))) seconds=400 samples=num_samples 
-            bench=run(benchset)
-            # times in seconds
-            results[(d,m)] = mean(bench).time*1000 
+            # Crear benchmarkable y correrlo
+            benchset = @benchmarkable sig($T, $signature_path, coef=$A, algorithm=$algorithm) seconds=400 samples=num_samples
+            bench = run(benchset)
+
+            # Guardar medianos en nanosegundos / bytes
+            wall_time[(d,m)] = median(bench).time / 1e9   
+            gc_time[(d,m)]   = median(bench).gctime / 1e9 
+            memory[(d,m)]    = median(bench).memory       
         end
     end
 
-    # Median matrix
-    median_matrix = zeros(length(ds), length(ms))
-    for ((d, m), times) in results
+
+    n_ds = length(ds)
+    n_ms = length(ms)
+    wall_matrix = zeros(n_ds, n_ms)
+    gc_matrix   = zeros(n_ds, n_ms)
+    mem_matrix  = zeros(n_ds, n_ms)
+
+    for ((d, m), t) in wall_time
         i = findfirst(==(d), ds)
         j = findfirst(==(m), ms)
-        median_matrix[i, j] = median(times)
+        wall_matrix[i,j] = t
+        gc_matrix[i,j]   = gc_time[(d,m)]
+        mem_matrix[i,j]  = memory[(d,m)]
     end
 
-    return median_matrix
+    return wall_matrix, gc_matrix, mem_matrix
 end
 
 # -------------------------------
-# Example
+# Ejemplo de uso
 # -------------------------------
-#ds = [10, 20, 30, 40, 50, 60]
-#ms = [10, 20, 30, 40, 50, 60]
+ds = [2, 3, 4]
+ms = [2, 3, 4]
 k = 3
 
-ds=[2,3]
-ms=[2,3]
-#ds 
-#ms
+wall, gc, mem = benchmark_signature(ds, ms, k, signature_path=:pwln, seq_type=:iis, num_samples=100)
 
-median_matrix = benchmark_signature(ds, ms, 3, signature_path=:pwln, seq_type=:iis, num_samples=100)
-println(median_matrix)
+
+println("Wall time (s):\n", wall)
+println("GC time (s):\n", gc)
+println("Memory (bytes):\n", mem)
